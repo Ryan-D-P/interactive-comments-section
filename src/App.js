9a9e9e79adcafb22cbace1eca3af47a2cf6b5bec
Lotus 98T <rpitty@live.com>
2022-02-02 17:33:44 +1000

    Added root comment posting. User state is set to show newly posted root comments.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import Attribution from './Attribution';
 import Comments from './Comments';
 import Post from './Post';
+import data from "./data.json";
+import { useState } from "react";
 
 function App() {
+  // State to store the comments data
+  const [userDataObj, setUserDataObj] = useState(data);
+
   return (
     <div className="App">
       <main className="main-content">
-        <Comments />
-        <Post />
+        <Comments userDataObj={ userDataObj } setUserDataObj={ setUserDataObj } />
+        <Post userDataObj={ userDataObj } setUserDataObj={ setUserDataObj } />
       </main>
 
       {/* <Attribution /> */}
